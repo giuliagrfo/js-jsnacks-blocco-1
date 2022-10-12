8db2404a99761ca67b961ee209bbac01388b7e0c
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-12 11:38:22 +0200

    print higher number

--- jsnack1/assets/js/main.js
+++ jsnack1/assets/js/main.js
@@ -4,3 +4,10 @@ Il software stampa il maggiore. */
 // L’utente inserisce due numeri in successione, con due prompt.
 const firstNumber = prompt('Scegli un numero');
 const secondNumber = prompt('Scegli un altro numero');
+
+
+if (firstNumber > secondNumber) {
+    console.log(firstNumber);
+} else {
+    console.log(secondNumber);
+}
