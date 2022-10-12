3c6a88aefc728f4bc4e0fc667dbe412668ba2811
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-12 12:03:10 +0200

    words request

---
+++ jsnack2/js/main.js
@@ -0,0 +1,8 @@
+/* L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.*/
+
+
+// L’utente inserisce due parole in successione, con due prompt.
+const firstWord = prompt('Scegli una parola');
+const secondWord = prompt('Scegli una seconda parola');
+
