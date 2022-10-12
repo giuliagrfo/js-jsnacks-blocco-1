0c5f7e12900bed378a2af239651d835cc2f4cc3b
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-12 11:35:20 +0200

    numbers request

---
+++ jsnack1/assets/js/main.js
@@ -0,0 +1,6 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+// L’utente inserisce due numeri in successione, con due prompt.
+const firstNumber = prompt('Scegli un numero');
+const secondNumber = prompt('Scegli un altro numero');
