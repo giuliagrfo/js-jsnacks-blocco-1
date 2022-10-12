87944fca21cf22d9d24cd6d6a09656a39f6b148e
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-12 12:44:01 +0200

    final commit

---
+++ jsnack3/js/main.js
@@ -0,0 +1,22 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.  */
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero. 
+const n1 = Number(prompt('Inserisci un numero'));
+const n2 = Number(prompt('Inserisci un numero'));
+const n3 = Number(prompt('Inserisci un numero'));
+const n4 = Number(prompt('Inserisci un numero'));
+const n5 = Number(prompt('Inserisci un numero'));
+const n6 = Number(prompt('Inserisci un numero'));
+const n7 = Number(prompt('Inserisci un numero'));
+const n8 = Number(prompt('Inserisci un numero'));
+const n9 = Number(prompt('Inserisci un numero'));
+const n10 = Number(prompt('Inserisci un numero'));
+
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+let somma;
+
+for (i = 0; i < n10; i++) {
+
+}
